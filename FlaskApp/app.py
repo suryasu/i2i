b83fef7c726618f5009a3a0d2ddd7a9b30a7bebd
Vivian He <vivian.he@caltech.edu>
2016-04-13 22:00:51 -0700

    added project homepage and create project

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -5,7 +5,19 @@ app = Flask(__name__)
 def main():
     #return "Welcome to I2I NOOB!"
     return render_template('index.html')
-    
+
+@app.route('/showSignUp')
+def showSignUp():
+    return render_template('signup.html')
+
+@app.route('/projectHome')
+def projectHome():
+    return render_template('project.html')
+
+@app.route('/createProject')
+def createProject():
+    return render_template('createproject.html')
+
 if __name__ == "__main__":
     app.run()
     
