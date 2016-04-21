c8d481afb3e64195fe538a653ecc4f8ccb6e4bf6
jagriti-agrawal28 <jagrawal268@gmail.com>
2016-04-21 10:11:03 -0700

    added to signup page

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, json, request
 app = Flask(__name__)
 
 @app.route("/")
@@ -9,6 +9,23 @@ def main():
 @app.route('/showSignUp')
 def showSignUp():
     return render_template('signup.html')
+ 
+@app.route('/signUp',methods=['POST'])
+def signUp():
+ 
+    # read the posted values from the UI
+    _name = request.form['inputName']
+    _email = request.form['inputEmail']
+    _password = request.form['inputPassword']
+    print _name
+ 
+    # validate the received values
+    if _name and _email and _password:
+        print "Hi"
+        return json.dumps({'html':'<span>All fields good !!</span>'})
+    else:
+        print "Bye"
+        return json.dumps({'html':'<span>Enter the required fields</span>'})
 
 @app.route('/projectHome')
 def projectHome():
@@ -17,6 +34,8 @@ def projectHome():
 @app.route('/createProject')
 def createProject():
     return render_template('createproject.html')
+    
+
 
 if __name__ == "__main__":
     app.run()
