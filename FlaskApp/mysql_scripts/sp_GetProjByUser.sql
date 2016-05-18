d49511eae30981fc74413a2d6c6cc93e24ef66da
Vivian He <vivian.he@caltech.edu>
2016-05-18 04:06:23 -0700

    Updated getMyProjects script

--- FlaskApp/mysql_scripts/sp_GetProjByUser.sql
+++ FlaskApp/mysql_scripts/sp_GetProjByUser.sql
@@ -7,7 +7,9 @@ CREATE PROCEDURE `sp_GetProjByUser` (
 IN p_user_id bigint
 )
 BEGIN
-    select * from tbl_projects where proj_user_id = p_user_id;
+    select proj_id,proj_title, proj_category, proj_completion_time, proj_n_collaborators, proj_description, proj_tags,
+    proj_user_id, proj_date, proj_file_path, getSum2(proj_id), hasLiked(proj_id,p_user_id) 
+    from tbl_projects where p_user_id = proj_user_id;
 END$$
  
 DELIMITER ;
