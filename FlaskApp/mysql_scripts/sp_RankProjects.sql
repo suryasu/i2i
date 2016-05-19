72231bc12efe49a7237964fffb42f826defbfc9d
Manasa <mashok@caltech.edu>
2016-05-18 21:13:38 -0700

    Fixed things
    added in ranking algorithm

---
+++ FlaskApp/mysql_scripts/sp_RankProjects.sql
@@ -0,0 +1,18 @@
+USE `BucketList`;
+DROP procedure IF EXISTS `sp_RankProject`;
+ 
+DELIMITER $$
+USE `BucketList`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_RankProjects`(
+	p_user int
+)
+BEGIN
+	/*select proj_id,proj_tags
+    from tbl_projects where p_user != proj_user_id;*/
+    
+    select proj_id,proj_title, proj_category, proj_completion_time, proj_n_collaborators, proj_description, proj_tags,
+    proj_user_id, proj_date, proj_file_path, getSum2(proj_id), hasLiked(proj_id,p_user) 
+    from tbl_projects where p_user != proj_user_id;
+END$$
+ 
+DELIMITER ;
