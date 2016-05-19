67eeba812f78fdcd9d42d1be4cd3d8c3270d35ff
Manasa <mashok@caltech.edu>
2016-05-19 16:54:51 -0700

    Script to add 145 projects to table

---
+++ FlaskApp/CS145ProjectInfo.sql
@@ -0,0 +1,51 @@
+/*INSERT INTO Bucketlist.tbl_projects(
+	proj_title,
+	proj_category,
+	proj_completion_time,
+	proj_n_collaborators,
+	proj_description,
+	proj_tags,
+    proj_user_id)
+VALUES
+	('Discovr', 'Tools', '3-6 months', '1-5', 'Personalized activity recommendation system', 'Cloud Computing, Web Design, UI Design, Networks and Communication', 11);
+*/
+/*
+INSERT INTO Bucketlist.tbl_projects(
+	proj_title,
+	proj_description,
+	proj_tags,
+    proj_user_id)
+VALUES
+	('Turnip', 
+    'Translate ideas and onlien communication into actual meetups', 
+    'Cloud/Distributed Computing, Web Design, UI Design, Networks and Communication', 
+    11),
+    ('Portal', 
+    'Browser extension to get dynamically updated network of important websites', 
+    'Keyword Matching, Web Design, UI Design, Networks and Communication', 
+    11),
+    ('Bender', 
+    'Distributed Encrypted Messaging Platform', 
+    'Encryption, Security, Distributed, Networks and Communication, Data', 
+    11);
+*/
+
+INSERT INTO Bucketlist.tbl_projects(
+	proj_title,
+	proj_description,
+	proj_tags,
+    proj_user_id)
+VALUES
+	('DNS Tunneling', 
+    'Tunneling over the DNS protocol', 
+    'TCP, Networking, Mobile', 
+    11),
+    ('University Ranking Algorithm', 
+    'Rankings via Faculty relations', 
+    'Machine Learning, Data Mining, Recommendation System, Page Rank, Networks', 
+    11),
+    ('i2i', 
+    'Idea to Implementation: Find cool projects to work on', 
+    'Machine Learning/ Data Mining, Recommendation System, Web Design, UI Design, Networks, Crowdsourcing', 
+    11);
+  SELECT * FROM Bucketlist.tbl_projects;
