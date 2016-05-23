a3b32d5c04b7c83248216656e5a42de036d0bfeb
suryasu <suryasu@users.noreply.github.com>
2016-05-23 00:10:44 -0700

    Changed Password
    Sorry need to for server to work

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -18,7 +18,7 @@ app.secret_key = 'why would I tell you my secret key?'
  
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'dolphin123'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'jordanbonilla'
 app.config['MYSQL_DATABASE_DB'] = 'bucketlist'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
