d562a0d75f68f0643a4e61109bfcc2748a5a2a30
suryasu <suryasu@users.noreply.github.com>
2016-05-23 03:26:53 -0700

    Tried changing ports

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -18,7 +18,7 @@ app.secret_key = 'why would I tell you my secret key?'
  
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'hihi1080'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'jordanbonilla'
 app.config['MYSQL_DATABASE_DB'] = 'bucketlist'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
@@ -1056,4 +1056,4 @@ def addAboutMe():
     
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False, port=59)
