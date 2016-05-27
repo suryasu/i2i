3a1446cd659e391141e507a9b38f57bb0560f337
Vivian He <vivian.he@caltech.edu>
2016-05-26 18:20:48 -0700

    Updated project homepage views'

--- FlaskApp/mysql_scripts/sp_GetProjById.sql
+++ FlaskApp/mysql_scripts/sp_GetProjById.sql
@@ -4,11 +4,11 @@ DROP procedure IF EXISTS `sp_GetProjById`;
 DELIMITER $$
 USE `BucketList`$$
 CREATE PROCEDURE `sp_GetProjById` (
-IN p_proj_id bigint
+IN p_proj_id bigint, p_user_id bigint
 )
 BEGIN
     select proj_id,proj_title, proj_category, proj_completion_time, proj_n_collaborators, proj_description, proj_tags,
-    proj_user_id, proj_date, proj_file_path, getSum2(proj_id)
+    proj_user_id, proj_date, proj_file_path, getSum2(proj_id), hasLiked(proj_id,p_user_id) 
     from tbl_projects where p_proj_id = proj_id;
 END$$
  
--- FlaskApp/mysql_scripts/tbl_collaborators.sql
+++ FlaskApp/mysql_scripts/tbl_collaborators.sql
@@ -1,5 +1,5 @@
  CREATE TABLE `tbl_collaborators` (
   `proj_id` bigint(20) NOT NULL,
-  `user_id` varchar(255) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
   PRIMARY KEY(`proj_id`, `user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
